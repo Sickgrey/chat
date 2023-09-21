5987f4fc8f565d06705f67b8fea5807620634b20
Sergey <fangooner.ars@gmail.com>
2023-09-21 13:32:23 +0200

    base dialog added

--- lib/ui_kit/ui_kit.dart
+++ lib/ui_kit/ui_kit.dart
@@ -4,5 +4,6 @@ import 'package:flutter/material.dart';
 
 part 'buttons/app_outlined_button.dart';
 part 'colors/app_colors.dart';
+part 'dialogs/base_dialog.dart';
 part 'input/app_text_field.dart';
 part 'theme/app_theme.dart';
